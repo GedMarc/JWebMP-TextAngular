fdac72766a80701ca059b1934959083902a438ea
Marc Magon <ged_marc@hotmail.com>
2018-01-20 21:14:27 +0200

    Grouped Async Startup for Post Loads, Angular File Watcher Initial

--- test/za/co/mmagon/jwebswing/plugins/textangular/TextAngularPageConfiguratorTest.java
+++ test/za/co/mmagon/jwebswing/plugins/textangular/TextAngularPageConfiguratorTest.java
@@ -3,22 +3,24 @@ package za.co.mmagon.jwebswing.plugins.textangular;
 import org.junit.jupiter.api.Test;
 import za.co.mmagon.jwebswing.BaseTestClass;
 import za.co.mmagon.jwebswing.Page;
+import za.co.mmagon.jwebswing.base.angular.AngularPageConfigurator;
 
 /**
  * @author Marc Magon
  */
 public class TextAngularPageConfiguratorTest extends BaseTestClass
 {
-	
+
 	public TextAngularPageConfiguratorTest()
 	{
 	}
-	
+
 	@Test
 	public void testSomeMethod()
 	{
+		AngularPageConfigurator.setRequired(true);
 		Page p = getInstance();
 		System.out.println(p.toString(0));
 	}
-	
+
 }
