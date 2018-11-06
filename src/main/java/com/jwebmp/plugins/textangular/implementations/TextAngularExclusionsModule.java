cd7f807f1be61459471e3977880b08cd92ff3912
GedMarc <ged_marc@hotmail.com>
2018-11-06 21:29:28 +0200

    JRE 11 full split and module specifications a bit stricter

---
+++ src/main/java/com/jwebmp/plugins/textangular/implementations/TextAngularExclusionsModule.java
@@ -0,0 +1,30 @@
+package com.jwebmp.plugins.textangular.implementations;
+
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
+
+import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Set;
+
+public class TextAngularExclusionsModule
+		implements IGuiceScanModuleExclusions<TextAngularExclusionsModule>,
+				           IGuiceScanJarExclusions<TextAngularExclusionsModule>
+{
+
+	@Override
+	public @NotNull Set<String> excludeJars()
+	{
+		Set<String> strings = new HashSet<>();
+		strings.add("jwebmp-text-angular-*");
+		return strings;
+	}
+
+	@Override
+	public @NotNull Set<String> excludeModules()
+	{
+		Set<String> strings = new HashSet<>();
+		strings.add("com.jwebmp.plugins.textangular");
+		return strings;
+	}
+}
