9f0c6424aa94df678a3b822c959e9126591c3ac6
GedMarc <ged_marc@hotmail.com>
2019-01-27 10:14:27 +0200

    module-info removal of import statements

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -1,11 +1,3 @@
-import com.jwebmp.core.base.angular.services.IAngularModule;
-import com.jwebmp.core.services.IPageConfigurator;
-import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
-import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
-import com.jwebmp.plugins.textangular.TextAngularModule;
-import com.jwebmp.plugins.textangular.TextAngularPageConfigurator;
-import com.jwebmp.plugins.textangular.implementations.TextAngularExclusionsModule;
-
 module com.jwebmp.plugins.textangular {
 
 	exports com.jwebmp.plugins.textangular;
@@ -19,10 +11,10 @@ module com.jwebmp.plugins.textangular {
 	requires com.jwebmp.guicedinjection;
 	requires com.jwebmp.core.angularjs;
 
-	provides IPageConfigurator with TextAngularPageConfigurator;
-	provides IAngularModule with TextAngularModule;
-	provides IGuiceScanModuleExclusions with TextAngularExclusionsModule;
-	provides IGuiceScanJarExclusions with TextAngularExclusionsModule;
+	provides com.jwebmp.core.services.IPageConfigurator with com.jwebmp.plugins.textangular.TextAngularPageConfigurator;
+	provides com.jwebmp.core.base.angular.services.IAngularModule with com.jwebmp.plugins.textangular.TextAngularModule;
+	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.textangular.implementations.TextAngularExclusionsModule;
+	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.textangular.implementations.TextAngularExclusionsModule;
 
 	opens com.jwebmp.plugins.textangular to com.fasterxml.jackson.databind, com.jwebmp.core;
 }
