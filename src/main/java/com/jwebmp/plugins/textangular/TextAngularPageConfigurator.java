d6e146128566ec5d2f5ec9b066ca3406474ba61d
GedMarc <ged_marc@hotmail.com>
2019-02-09 11:52:53 +0200

    The Website API Brings out many things....

--- src/main/java/com/jwebmp/plugins/textangular/TextAngularPageConfigurator.java
+++ src/main/java/com/jwebmp/plugins/textangular/TextAngularPageConfigurator.java
@@ -89,7 +89,7 @@ public class TextAngularPageConfigurator
 	@Override
 	public Page configure(Page page)
 	{
-		if (!page.isConfigured())
+		if (!page.isConfigured() && enabled())
 		{
 			JQueryPageConfigurator.setRequired(true);
 			AngularPageConfigurator.setRequired(true);
