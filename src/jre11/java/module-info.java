2850841468d89c80da2ca8c2fc91f58751888dcc
GedMarc <ged_marc@hotmial.com>
2020-03-08 16:59:55 +0200

    Remove Jar syntax as JDK 8 is no longer supported

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -14,7 +14,6 @@ module com.jwebmp.plugins.textangular {
 	provides com.jwebmp.core.services.IPageConfigurator with com.jwebmp.plugins.textangular.TextAngularPageConfigurator;
 	provides com.jwebmp.core.base.angular.services.IAngularModule with com.jwebmp.plugins.textangular.TextAngularModule;
 	provides com.guicedee.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.textangular.implementations.TextAngularExclusionsModule;
-	provides com.guicedee.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.textangular.implementations.TextAngularExclusionsModule;
 
 	opens com.jwebmp.plugins.textangular to com.fasterxml.jackson.databind, com.jwebmp.core;
 }
--- src/main/java/com/jwebmp/plugins/textangular/implementations/TextAngularExclusionsModule.java
+++ src/main/java/com/jwebmp/plugins/textangular/implementations/TextAngularExclusionsModule.java
@@ -1,6 +1,5 @@
 package com.jwebmp.plugins.textangular.implementations;
 
-import com.guicedee.guicedinjection.interfaces.IGuiceScanJarExclusions;
 import com.guicedee.guicedinjection.interfaces.IGuiceScanModuleExclusions;
 
 import javax.validation.constraints.NotNull;
@@ -8,18 +7,8 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class TextAngularExclusionsModule
-		implements IGuiceScanModuleExclusions<TextAngularExclusionsModule>,
-				           IGuiceScanJarExclusions<TextAngularExclusionsModule>
+		implements IGuiceScanModuleExclusions<TextAngularExclusionsModule>
 {
-
-	@Override
-	public @NotNull Set<String> excludeJars()
-	{
-		Set<String> strings = new HashSet<>();
-		strings.add("jwebmp-text-angular-*");
-		return strings;
-	}
-
 	@Override
 	public @NotNull Set<String> excludeModules()
 	{
