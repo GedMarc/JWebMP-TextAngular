8c2ed78f74c17fd5478cc39f0bee76ae07d1dd4f
GedMarc <ged_marc@hotmail.com>
2020-11-12 01:19:16 +0200

    Jakarta Namespace - JLink building

--- src/main/java/com/jwebmp/plugins/textangular/TextAngularPageConfigurator.java
+++ src/main/java/com/jwebmp/plugins/textangular/TextAngularPageConfigurator.java
@@ -23,7 +23,7 @@ import com.jwebmp.core.plugins.PluginInformation;
 import com.jwebmp.core.plugins.jquery.JQueryPageConfigurator;
 import com.jwebmp.core.services.IPageConfigurator;
 
-import javax.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotNull;
 
 /**
  * The Angular UI Sortable Configurator
